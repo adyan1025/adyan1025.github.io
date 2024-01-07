5434838795b8c703e34c207e1ecd9492509cff86
Adyan Chowdhury <chowdhuryadyan13@gmail.com>
2024-01-06 21:50:36 -0500

    Added props for cards

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,16 @@
-function Card({title, body}) {
+import PropTypes from 'prop-types'
+
+function Card(props) {
     return (
         <div className="cards">
-            <h1 className="card_heading">{title}</h1>
-            <p>{body}</p>
+            <h1 className="card_heading">{props.title}</h1>
+            <p>{props.body}</p>
         </div>
     );
+}
+
+Card.propTypes = {
+    title: PropTypes.string,
 }
 
 export default Card
