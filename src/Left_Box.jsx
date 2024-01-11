47742e4d589f29e4b924a26eec7fda524efbbd3b
Adyan Chowdhury <chowdhuryadyan13@gmail.com>
2024-01-11 00:27:38 -0500

    Adjusted sizing, added to About Me and Skills, Updated Title

--- src/Left_Box.jsx
+++ src/Left_Box.jsx
@@ -6,7 +6,7 @@ function Left_Box() {
             <img src = {photo} className = "linkedin"></img>
             <h1 class="slide-in-bck-center" className = "name"><b>Adyan C.</b></h1>
             <p className = "subtext">
-            Temple University • College of Science and Technology • Computer Science • 19 years old • Programmer • UTC-5:00 
+            Temple University Honors • College of Science and Technology • Computer Science • 19 years old • Programmer • UTC-5:00 
             </p>
         </div>
     );
--- src/Right_Box.jsx
+++ src/Right_Box.jsx
@@ -7,15 +7,26 @@ function Right_Box() {
             <Card
                 title={'About Me'}
                 body={<div>Hi! My name is Adyan Chowdhury. I am a Bengali American who is currently
-                    studying Computer Science at Temple University's College of Science and Technology.</div>}
+                    studying Computer Science at Temple University's College of Science and Technology. 
+                    <br/><br/>I was born in Philly and grew up in the Upper Merion Township with both my parents
+                    coming from Bangladesh. I grew up in a Muslim household alongside my older sister. 
+                    <br/><br/>One of my pastimes would be playing videogames! I grew up in the generation revolving 
+                    the likes of Minecraft and such. However, my favorite game would have to be "UnderTale". This game in 
+                    particular inspired me to become a programmer. 
+                    <br/><br/>Another hobby I have would be creative writing! I have written numerous works in my free time 
+                    —including poems, stories, and other works of fiction. To that end, I have a future aspiration that would 
+                    involve combining my creating writing prowess with programming. Hopefully I am able to see that through!</div>}
             />
             <Card
                 title={'Skills'}
-                body={<div>I am familiar with C, HTML, CSS, Javascript and Python. I love learning and I am actively picking up new skills all the time.</div>}
+                body={<div>I am familiar with C, HTML, CSS, Javascript and Python. I love learning and I 
+                    am actively picking up new skills. <br/><br/>Currently, I am in the process of mastering Java as my 
+                    latest programming language. Additionally, I engage in hands-on projects using Godot's Game Engine, 
+                    exploring the realm of game development as a side pursuit.</div>}
             />
             <Card
                 title={'Projects'}
-                body={<div>You can find most projects listed on my résumé or on my Github. Feel free to check them out!</div>}
+                body={<div>You can find all projects on my Github and my most notable ones on my résumé. Feel free to check them out!</div>}
             />
             <Card
                 title={'Links'}
