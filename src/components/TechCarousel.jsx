e9e7b9e859d3f5713f89eaa89cac3749f857a677
adyan1025 <chowdhuryadyan13@gmail.com>
2026-05-30 14:50:38 -0400

    Bug Fix: Remove pause and play buttons in tech stack carousel

--- src/components/TechCarousel.jsx
+++ src/components/TechCarousel.jsx
@@ -42,8 +42,6 @@ function TechCarousel() {
     <FaRedhat />,
     <FaGitAlt />,
     <FaDocker />,
-    <FaPauseCircle />,
-    <FaPlayCircle />,
     <SiPostgresql />,
     <SiTypescript />,
     <FaBootstrap />,
