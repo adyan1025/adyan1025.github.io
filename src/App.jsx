5aa65f2e9bf9e56c0d2b5354900fb874b60186aa
adyan1025 <chowdhuryadyan13@gmail.com>
2026-05-30 03:04:18 -0400

    Cleaned landing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,6 @@ function App() {
     <>
       <Navbar />
       <Intro />
-      <h1>adyan chowdhury</h1>
-      <h2>software engineer</h2>
-      <h3>projects</h3>
-      <h4>coming soon</h4>
-      <p>Coming soon.</p>
     </>
   );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,23 @@
 import '../styling/Intro.css'
+import AdyanPhoto from '../assets/landing-pic.png'
+import RedUnderline from '../assets/red-underline.png'
 
 function Intro() {
     return(
         <div className="intro">
-            <h1>This is the Intro</h1>
-            <h2>of the Website</h2>
+            <div className='intro-text'>
+                <header>
+                    <div className='h1-with-underline'>
+                        <h1>adyan chowdhury</h1>
+                        <img src={RedUnderline} alt="Red Underline" />
+                    </div>
+                    <h2>21 yo • computer science • undergrad student • cloud engineer</h2>
+                </header>
+                <h3>I am a current 4th year undergraduate student studying at Temple University majoring in computer science and minoring in data science. My work included full-stack application, dev-ops, and cloud infrastructure.</h3>
+            </div>
+            <div className='intro-image'>
+                <img src={AdyanPhoto} alt="Adyan Chowdhury" />
+            </div>
         </div>
     );
 }
