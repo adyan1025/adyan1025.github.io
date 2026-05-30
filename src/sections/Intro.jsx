b2e371523f631855ad9bf4efcc010c5c5ee9c052
adyan1025 <chowdhuryadyan13@gmail.com>
2026-05-30 15:17:17 -0400

    Updated intro

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -11,9 +11,9 @@ function Intro() {
                         <h1>adyan chowdhury</h1>
                         <img src={RedUnderline} alt="Red Underline" />
                     </div>
-                    <h2>21 yo • computer science • undergrad student • cloud engineer</h2>
+                    <h2>computer science • undergrad student • cloud engineer</h2>
                 </header>
-                <h3>I am a current 4th year undergraduate student studying at Temple University majoring in computer science and minoring in data science. My work included full-stack application, dev-ops, and cloud infrastructure.</h3>
+                <h3>I am a current 4th year undergraduate student studying at Temple University majoring in computer science and minoring in data science. My work included full-stack application, dev-ops, machine learning, artificial intelligence, and cloud infrastructure.</h3>
             </div>
             <div className='intro-image'>
                 <img src={AdyanPhoto} alt="Adyan Chowdhury" />
